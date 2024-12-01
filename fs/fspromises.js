0fc3854f11c3a46ee6a9b528dd04692b179e13ee
chetanv18~ <chetan@gully2global.com>
2024-12-01 19:02:34 +0530

    new

---
+++ fs/fspromises.js
@@ -0,0 +1,34 @@
+const { error } = require("console");
+const fs =require("fs");
+const path = require("path");
+
+const filename = "fsprimises.text";
+const filePath = path.join(__dirname, filename);
+
+
+// const file = __dirname;
+// fs.promises.readdir(file)
+// .then((data) => console.log(data))
+// .catch((error)=> console.log(error));
+
+
+
+// const fileName1 = __dirname;
+// fs.promises.readdir(fileName1)
+// .then((data) => console.log(data))
+// .catch((error)=> console.log(error));
+
+// write file
+// fs.promises.writeFile(filePath, "this is intials data", "utf-8")
+// .then(console.log("file created"))
+// .catch((error)=> console.log(error));
+
+//  read file
+
+
+
+fs.promises
+.readFile(filePath, "utf-8")
+.then((data)=>console.log(data))
+.catch((error)=> console.error("error reading file :", error));
+
