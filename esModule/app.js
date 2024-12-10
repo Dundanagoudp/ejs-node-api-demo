5837b3eb4a59f1901d3124145fec6428bdc60559
chetanv18~ <chetan@gully2global.com>
2024-12-10 22:43:42 +0530

    new

---
+++ esModule/app.js
@@ -0,0 +1,14 @@
+// const { add, sub ,PI} = require('./math.js');
+
+// console.log(add(5, 10)); 
+// console.log(sub(5, 10));
+// console.log(PI);
+
+
+// const mult = require("./math");
+
+// import mult from "./math.js";
+import { mult ,div } from "./math.js";
+console.log(mult(500, 10));
+console.log(div(5,10));
+// console.log(Math.PI);
---
+++ esModule/math.js
@@ -0,0 +1,27 @@
+// math.js
+
+// const PI = 3.14159;
+
+// function add(a, b) {
+//       return a + b;
+//   }
+  
+//   function sub(a, b) {
+//       return a * b;
+//   }
+  
+//   module.exports = { add, sub ,PI};
+  
+
+   const div = (a ,b)=>{
+    return a/ b;
+  };
+
+   const mult = (a, b)=>{
+    return a * b;
+  };
+
+  const PI = 3.214;
+
+  export {div ,mult};
+  // export default mult;
