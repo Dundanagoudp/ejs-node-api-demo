a6675d8e252f92a7b6a48b8449981da1847cee61
chetanv18~ <chetan@gully2global.com>
2025-01-10 20:58:53 +0530

    new

--- mini_project/url/app.js
+++ mini_project/url/app.js
@@ -1,33 +1,15 @@
-import { readFile } from "fs/promises";
-import {createServer} from "http";
-import path from "path";
+import express from "express";
+const app = express();
+import router from "../router/shortener.routes";
 
-const PORT = 3000;
 
-const serveFile = async(res, filePath, ContentType) =>{
-      try {
-            const data = await readFile(filePath);  
-            res.writeHead(200,{'Content-Type':ContentType});
-            res.end(data);
-         } catch (error) {
-               res.writeHead(404,{'Content-Type':ContentType});
-               res.end("404 page not found");
-         }
-}
+const PORT = process.env.PORT || 3000;
 
-const server = createServer(async(req,res)=>{
-      console.log(req.url);
-      if(req.method === "GET") {
-            if(req.url === "/"){
-                 return serveFile(res,
-                  path.join("public", "index.html" ),"text/html");
-            } else if (req.method === "GET") {
-                  if(req.url === "/style.css"){
-                        return serveFile(res, path.join("public", "style.css" ),"text/css");
-                  }
-      }
-}});
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-server.listen(PORT, ()=>{
-console.log(`server is running on port https://localhost:${PORT}`);
-})
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
